dc7f6e04e02e8d5125582265e5b00db5d53c6c4a
Dani Rodriguez <danielrodrifern@gmail.com>
2024-02-10 10:51:18 +0100

    Atributos class

--- src/ProgOrientadaObjetos/Persona.java
+++ src/ProgOrientadaObjetos/Persona.java
@@ -1,4 +1,13 @@
 package ProgOrientadaObjetos;
 
+import java.util.Date;
+
 public class Persona {
+    // Atributos
+    private String nombre;
+    private String apellido;
+    private Date fechaNacimiento;
+    private int edad;
+    private String dni;
+    private String direccion;
 }
